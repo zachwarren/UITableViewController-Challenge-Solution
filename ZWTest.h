d0b41202f79d9d400833c5c64d56613b3c8ef3b1
Zach Warren <zachwarren@zachs-mbp.gateway.pace.com>
2014-06-01 21:01:44 -0700

    added test

---
+++ ZWTest.h
@@ -0,0 +1,13 @@
+//
+//  ZWTest.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Zach Warren on 01/06/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZWTest : NSObject
+
+@end
---
+++ ZWTest.m
@@ -0,0 +1,13 @@
+//
+//  ZWTest.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Zach Warren on 01/06/2014.
+//
+//
+
+#import "ZWTest.h"
+
+@implementation ZWTest
+
+@end
